e2c2426c1ff25d6ef71c8acc5dd07145c6c1e686
Fabio Maciel <fabio.souza@metasix.com.br>
2016-10-18 20:04:37 -0200

    Added mocha, chai and blue bird #18 and #23

--- src/models/db.js
+++ src/models/db.js
@@ -3,6 +3,8 @@
 const db = require('mongoose'),
       config = require('../../config/config')
       
+db.Promise = require('bluebird');
+
 db.connect(`mongodb://${config.mongo.host}/${config.mongo.database}`)
 
 module.export = db.connection
