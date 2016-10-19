f933d18d230419fc000b5d0548b43b88d753121e
Magno <owlflee@gmail.com>
2016-10-18 22:25:04 -0200

    Added a querystring to card route #30

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -1,14 +1,19 @@
 'use strict';
 const express = require('express'),
-      router = express.Router();
+      router  = express.Router(),
+      cards   = require('../services/cardService.js');
 
-router.get('/',(req,res) => {
-    res.send('JSON com todas cartas');
-});
 
 router.get('/:id', (req,res) => {
     res.send(`JSON com carta do id: ${req.params.id} `);
 });
 
+router.get('/', (req,res) => {
+    if(req.query.name.length > 0){
+        cards.findByName(new RegExp(req.query.name)).lean().exec((err,card)=>{
+            res.send(JSON.stringify(card));
+        });
+    }
+})
 
 module.exports = router;
