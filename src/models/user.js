718753fbb4024d0a0345152f9312f9fabd8c50cf
Magno Gouveia <owlflee@gmail.com>
2016-10-26 17:34:31 -0200

    Improve on user schema #36

--- src/models/user.js
+++ src/models/user.js
@@ -1,18 +1,17 @@
 'use strict'
 
 const mongoose = require('mongoose'),
-			db = require('./db')
-
+	  db       = require('./db');
 
 const UserSchema = mongoose.Schema({
 	username: {type: String, unique: true, dropDups: true, required: true},
 	password: {type: String, dropDups: true, required: true},
-	deckList: {type: Array, required: false},
+	deckList: [{name: {type: String}, cards: {type: Array} }],
 	lastLogin: {type: Date, default: Date.now} ,
+    type: {type: String, required: true, default: 'USER'},
     entryDate: Date,
     email: String
 })
-
 
 const User = mongoose.model('user', UserSchema)
 
