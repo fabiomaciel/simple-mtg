fefab4ef36e4198950cf738ced6484f04452574a
Hemanth Kumar Veeranki <hems.india1997@gmail.com>
2016-10-27 18:19:57 +0530

    Added Hash for password and signup controller

--- src/models/user.js
+++ src/models/user.js
@@ -5,12 +5,12 @@ const mongoose = require('mongoose'),
 
 const UserSchema = mongoose.Schema({
 	username: {type: String, unique: true, dropDups: true, required: true},
-	password: {type: String, dropDups: true, required: true},
 	deckList: [{name: {type: String}, cards: [ {quantity: Number, id: String} ] }],
 	lastLogin: {type: Date, default: Date.now} ,
     type: {type: String, required: true, default: 'USER'},
     entryDate: Date,
-    email: String
+    email: {type: String, unique: true, dropDups: true, required: true},
+    hash:String,
 })
 
 const User = mongoose.model('user', UserSchema)
--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,13 +1,36 @@
 'use strict';
 const express = require('express'),
 router  = express.Router();
-
+var service = require('../services/userService.js');
+var _ = require('lodash');
+var db = require('../models/user');
 router.get('/',(req,res) => {
   res.render('pages/signup');
 });
 
-router.post('/',(req,res) => {
-  res.render('pages/signup');
+router.post('/',(req,res) =>{
+	var data = req.body;
+	req.checkBody("email", "Enter a valid email address.").isEmail();
+	var error_str = [];
+  var errors = req.validationErrors();
+  if(data.confirm_password != data.password){
+    error_str.push('Password and Confirm Password doesnt match');
+  }
+  if (data.email != data.confirm_email){
+    error_str.push('Email and Confirm email doesnt match');
+  }
+  if (errors) {
+   for (var i = 0; i < errors.length; i++){
+    error_str.push(errors[i].msg);
+  }
+  res.render('pages/signup',{error:error_str});
+}
+else{
+  delete data.confirm_email;
+  delete data.confirm_password;
+  service.createUser(data,res,req,error_str);
+
+}
 });
 
 module.exports = router;
