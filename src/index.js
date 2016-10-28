ff3499db89e68c11d9e09e18b2a673b5807417bd
Magno Gouveia <owlflee@gmail.com>
2016-10-28 08:20:19 -0200

    Removed deprecated functions and aligned code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 'use strict';
-const express = require('express'),
-      app     = express(),
+const express    = require('express'),
+      app        = express(),
       bodyParser = require('body-parser'),
-      validator = require('express-validator'),
-      home    = require('./routes/home'),
-      register = require('./routes/signup'),
-      cards   = require('./routes/cards'),
-      decks   = require('./routes/decks'),
-      matches = require('./routes/matches'),
-      users   = require('./routes/users');
+      validator  = require('express-validator'),
+      home       = require('./routes/home'),
+      register   = require('./routes/signup'),
+      cards      = require('./routes/cards'),
+      decks      = require('./routes/decks'),
+      matches    = require('./routes/matches'),
+      users      = require('./routes/users');
 
 app.get('/health',function(req,res){
     res.send('It\'s ALIIIIIIVE');
 });
 
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(validator());
 
 app.use(express.static('public'));
